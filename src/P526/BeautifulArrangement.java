786cb096f78c25312edbbdcbb6bbfbaec755c100
maybefree <88603980+maybefree@users.noreply.github.com>
2022-05-10 00:25:32 +0800

    Add files via upload

---
+++ src/P526/BeautifulArrangement.java
@@ -0,0 +1,48 @@
+package P526;
+
+public class BeautifulArrangement {
+    public static int count = 0;
+    public static int countArrangement(int n) {
+
+        if (n == 0) {
+            return 0;
+        }
+
+        // 标记数组
+        int[] nums = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            nums[i] = i;
+        }
+
+        DFS(nums, n);
+        return count;
+    }
+
+    public static void DFS(int[] nums, int start) {
+        if (start == 0) {
+            count++;
+            return;
+        }
+
+        for (int i = start; i > 0; i--) {
+            //
+            int temp = nums[start];
+            nums[start] = nums[i];
+            nums[i] = temp;
+
+            //perm[i] 能够被 i 整除  i 能够被 perm[i] 整除
+            if (nums[start] % start == 0 || start % nums[start] == 0) {
+                DFS(nums, start - 1);
+            }
+
+            temp = nums[i];
+            nums[i] = nums[start];
+            nums[start] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(countArrangement(3));
+    }
+}
