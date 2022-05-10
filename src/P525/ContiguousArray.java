6c9a4b80198f10831a1deee9e656306fbacac68e
handong <wanghandong@zuoyebang.com>
2022-05-11 00:09:35 +0800

    525.连续数组

---
+++ src/P525/ContiguousArray.java
@@ -0,0 +1,35 @@
+package P525;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContiguousArray {
+
+    // https://leetcode.cn/problems/contiguous-array/solution/dong-tu-yan-shi-qian-zhui-he-si-xiang-by-z2no/
+    public static int findMaxLength(int[] nums) {
+
+        int cur = 0, ans = 0;
+        int length = nums.length;
+
+        Map<Integer, Integer> map = new HashMap<>(length, 1.05f);
+        map.put(0, -1);
+
+        for (int i = 0; i < length; i++) {
+            nums[i] = nums[i] == 0 ? --cur : ++cur;
+            if (map.containsKey(cur)) {
+
+                ans = Math.max(ans, i - map.get(cur));
+            } else {
+                map.put(cur, i);
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,1,0};
+        System.out.println(findMaxLength(nums));
+    }
+}
